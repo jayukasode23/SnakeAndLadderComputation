8614cf9a2c4f112ced70495a30448fd2b60f0635
jayukasode23 <jayukasode2398@gmail.com>
2022-10-06 19:44:30 +0530

    Added Ladder Or Snake

---
+++ SnakeAndLadder2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class SnakeAndLadder2
+    {
+        public static void SnakeLadder()
+        {
+            int position = 0;
+
+            Random random = new Random();
+
+            Console.WriteLine("Player starts at position: " + position);
+
+            int die = random.Next(1, 7);
+            position += die;
+
+            Console.WriteLine("Now at position " + position);
+        }
+    }
+}
+    
---
+++ SnakeAndLadder3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    internal class SnakeAndLadder3
+    {   
+            //Constant
+            public const int Noplay = 0;
+            public const int Ladder = 1;
+            public const int Snake = 2;
+
+
+
+            public static void SnakeLadder()
+            {
+                int position = 100;
+
+                Random random = new Random();
+
+                Console.WriteLine("Player starts at position: " + position);
+
+                int die = random.Next(1, 7);
+                int option = random.Next(0, 3);
+
+                switch (option)
+                {
+                    case Ladder:
+                        position += die;
+                        break;
+
+                    case Snake:
+                        position -= Snake;
+                        break;
+
+                    default:
+                        position = position;
+                        break;
+                }
+                Console.WriteLine(position);
+            }
+
+        }
+    }
+
+
